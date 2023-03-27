8b7e79ee17f9f3c3c2d612447c984c3302955322
ltobywongl <tohongwong@gmail.com>
2023-03-28 01:27:08 +0800

    add file for ssl

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -3,9 +3,9 @@ import bcrypt from "bcrypt"
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    bcrypt.hash(req.body.password, 10, async function (err, hash) {
-      console.log(hash)
-    });
+    // bcrypt.hash(req.body.password, 10, async function (err, hash) {
+    //   console.log(hash)
+    // });
     try {
       const result = await excuteQuery({
         query: 'SELECT * FROM user where email=?;',
